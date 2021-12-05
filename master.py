fbed53969aaf76d4b9816260d2ab696c09b3ad1e
petruciocirlan <petru.ciocirlan00@gmail.com>
2021-12-05 15:33:24 +0200

    Added rabbitmq queue (master to worker); refactoring; argument parsing; multiple worker processes

--- master.py
+++ master.py
@@ -1,54 +1,73 @@
 from bs4 import BeautifulSoup
+import subprocess
 import traceback
 import json
 import sys
 import os
 
-from common import get_page_content, DEBUG_WORKER_INPUT_FILE
+from common import get_page_content, open_rabbitmq_channel
+from common import parse_arguments, DEFAULT_RABBITMQ_QUEUE_NAME
+from common import DEBUG_WORKER_INPUT_FILE
+
+# TODO(@pciocirlan): improve exceptions
+# TODO(@pciocirlan): add logger (to .log file)
+# TODO(@pciocirlan): PEP documentation
+
+# flag -> (argument count, default)
+OPTIONS = {
+    "--test": (0, False),
+    "--queue": (1, DEFAULT_RABBITMQ_QUEUE_NAME),
+    "--worker-count": (1, 16)
+}
 
 TOPSITE_URL = "https://www.alexa.com/topsites/"
 
 UNRESPONSIVE_COUNTRY_PAGES = [
-    "Aland Islands", # just added (refreshed topsites and newly appeared, but link is down at the moment)
+    # just added (refreshed topsites and newly appeared, but link is down at the moment)
+    "Aland Islands",
 ]
 
-# TODO(@pciocirlan): improve exceptions
-# TODO(@pciocirlan): add logger (to .log file)
+open_subprocesses = list()
+settings = {}
 
 def main():
+    global settings
+    settings = parse_arguments(sys.argv[1:], OPTIONS)
+
     try:
         country_pages_links = get_country_pages_links()
     except Exception as e:
         raise Exception(f"Get country links: {e}")
 
+    conn, ch = None, None
+    if not settings["--test"]:
+        conn, ch = open_rabbitmq_channel(settings["--queue"], clear_queue=True)
+
+        global open_subprocesses
+        for _ in range(settings["--worker-count"]):
+            proc = subprocess.Popen(["python", "worker.py"])
+            open_subprocesses.append(proc)
+
     for link in country_pages_links:
         if link['country'] in UNRESPONSIVE_COUNTRY_PAGES:
             continue
 
-        print(f"Looking at {link['country']} top sites ... ", end="", flush=True)
-        top_50_sites = get_top_country_sites(link['url'])
+        print(
+            f"Looking at {link['country']} top sites ... ", end="", flush=True)
+        try:
+            top_sites = get_top_country_sites(link['url'])
+        except Exception as e:
+            print(f"failed: {e}")
+            continue
         print("done!")
 
-        jsons = list()
-        for rank, website in enumerate(top_50_sites, 1):
-            filename = f"%(index)02d %(website)s.html" % {
-                "index": rank,
-                "website": website
-                }
+        send_tasks(ch, link['country'], top_sites)
 
-            # "{country}/{index} {website}.html"
-            filepath = os.path.join("topsites", link['country'], filename)
-            website = f"http://{website}"
+    if not settings["--test"]:
+        send_stop_messages(ch)
 
-            jsons += [{
-                "filepath": filepath,
-                "website": website
-                }]
-
-        if "--test" in sys.argv:
-            with open(DEBUG_WORKER_INPUT_FILE, "w") as fd:
-                json.dump(jsons, fd, indent=4)
-                exit(0)
+    if conn is not None:
+        conn.close()
 
 
 def get_country_pages_links():
@@ -62,7 +81,8 @@ def get_country_pages_links():
     for country_list in country_lists:
         links = country_list.find_all("a")
 
-        country_links += [{"country": link.text, "url": TOPSITE_URL + link['href']} for link in links]        
+        country_links += [{"country": link.text,
+                           "url": TOPSITE_URL + link['href']} for link in links]
 
     return country_links
 
@@ -86,8 +106,61 @@ def get_top_country_sites(url):
     return top_sites
 
 
+def send_tasks(ch, folder_name, top_sites):
+    jsons = list()
+    for rank, website in enumerate(top_sites, 1):
+        filename = f"%(index)02d %(website)s.html" % {
+            "index": rank,
+            "website": website
+        }
+
+        # "topsites/{country}/{index} {website}.html"
+        filepath = os.path.join("topsites", folder_name, filename)
+        website = f"http://{website}"
+
+        obj = {
+            "Link": website,
+            "LocatieDisk": filepath
+        }
+
+        jsons += [obj]
+
+        if ch is not None:
+            obj_json = json.dumps(obj)
+
+            ch.basic_publish(
+                exchange='', routing_key=settings["--queue"], body=obj_json)
+
+            print(f"Sent {obj_json} to queue!")
+
+    if settings["--test"]:
+        with open(DEBUG_WORKER_INPUT_FILE, "w") as fd:
+            json.dump(jsons, fd, indent=4)
+            exit(0)
+
+
+def send_stop_messages(ch):
+    for _ in range(settings["--worker-count"]):
+        obj_json = json.dumps({"action": "STOP"})
+
+        ch.basic_publish(
+            exchange='', routing_key=settings["--queue"], body=obj_json)
+
+
+def kill_subprocesses():
+    global open_subprocesses
+    for proc in open_subprocesses:
+        proc.kill()
+
+
 if __name__ == "__main__":
     try:
         main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        kill_subprocesses()
+        exit(0)
     except Exception as e:
         print(traceback.format_exc())
+        kill_subprocesses()
+        exit(-1)
--- worker.py
+++ worker.py
@@ -4,33 +4,77 @@ import json
 import sys
 import os
 
-from common import get_page_content, DEBUG_WORKER_INPUT_FILE
+from common import get_page_content, open_rabbitmq_channel
+from common import parse_arguments, DEFAULT_RABBITMQ_QUEUE_NAME
+from common import DEBUG_WORKER_INPUT_FILE
+
+# flag -> (argument count, default)
+OPTIONS = {
+    "--test": (0, False),
+    "--queue": (1, DEFAULT_RABBITMQ_QUEUE_NAME)
+}
+
+settings = {}
+
 
 def main():
-    data = None
-    if "--test" in sys.argv:
+    global settings
+    settings = parse_arguments(sys.argv[1:], OPTIONS)
+
+    if settings["--test"]:
         with open(DEBUG_WORKER_INPUT_FILE, "r") as fd:
             data = json.load(fd)
 
-    for top_site in data:
-        try:
-            download_to_disk(top_site)
-        except Exception as e:
-            print(f"Failed to download website {top_site['website']}: {e}")
+        for top_site in data:
+            try:
+                download_to_disk(top_site)
+            except Exception as e:
+                print(f"Failed to download website {top_site['website']}: {e}")
+
+        exit(0)
+
+    conn, ch = open_rabbitmq_channel(settings["--queue"])
+    ch.basic_qos(prefetch_count=1)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    ch.basic_consume(
+        queue=settings["--queue"], auto_ack=False, on_message_callback=callback_rabbitmq)
+    ch.start_consuming()
 
 
 def download_to_disk(info):
-    dirname = os.path.dirname(info['filepath'])
+    dirname = os.path.dirname(info['LocatieDisk'])
     os.makedirs(dirname, exist_ok=True)
 
-    web_page = get_page_content(info['website'])
+    web_page = get_page_content(info['Link'])
 
-    with open(info['filepath'], "w", encoding="utf8") as fd:
+    with open(info['LocatieDisk'], "w", encoding="utf8") as fd:
         fd.write(web_page)
+
+
+def callback_rabbitmq(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+    message = json.loads(body)
+
+    if "action" in message and message["action"] == "STOP":
+        print(" [x] STOP message received. Stopped.")
+        exit(0)
+
+    try:
+        download_to_disk(message)
+    except Exception as e:
+        print(f"Failed to download website {message['Link']}: {e}")
+    finally:
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 if __name__ == "__main__":
     try:
         main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        exit(0)
     except Exception:
         print(traceback.format_exc())
+        exit(-1)
